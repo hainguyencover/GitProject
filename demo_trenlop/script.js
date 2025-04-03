ffcba8fa0503b8ec10bbfeec40c1825cfdcd05f8
hainguyencover <haiconk95@gmail.com>
2025-04-03 10:31:43 +0700

    nop bai tap lesson12_mang1

--- demo_trenlop/script.js
+++ demo_trenlop/script.js
@@ -1,10 +1,15 @@
-// // script.js
-// document.getElementById('myButton').addEventListener('click', function() {
-//     var message = "Hello, world!";
-//     console.log(message);
-// });
-// function calculateSum(a, b) {
-//     return a + b;
+// let one = [2, 4, 6];
+// let two = new Array(one.length + 1);
+// for (let i = 0; i < one.length; i++) {
+//     two[i] = one[i];
 // }
-// console.log(calculateSum(5, 10))
-// // calculateSum(5, "10");
+// // two[one.length] = 7;
+// two[two.length - 1] = 7;
+// alert(two);
+
+
+let girls = ["Lan", "Hoa", "Dao", "Huong"];
+let boys = ["Ti", "Teo", "Tuat", "Hoi"];
+for (let i = 0; i < girls.length; i++) {
+    console.log(girls[i] + " " + boys[Math.floor(Math.random() * i)])
+}
